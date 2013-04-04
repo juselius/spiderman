fd7814ab1ba51ad6582b0356e9148351d54f5e48
Jonas Juselius <jonas.juselius@uit.no>
2013-04-04 10:23:15 +0200

    Added script to run spider and clean up result.

---
+++ runspider.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+spider=/opt/lmod/lmod/libexec/spider
+
+$spider -o softwarePage $1 |\
+    iconv -f ISO8859-1 -t UTF8 |\
+    sed "s/[\]'/'/g" |\
+    python -mjson.tool
+#    jsonlint -f
