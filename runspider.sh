6a03ae7e9af0e47035b4b8726a05251656b51298
Jonas Juselius <jonas.juselius@uit.no>
2013-04-04 14:53:13 +0200

    Removed JSON pretty printer.

--- runspider.sh
+++ runspider.sh
@@ -4,6 +4,6 @@ spider=/opt/lmod/lmod/libexec/spider
 
 $spider -o softwarePage $1 |\
     iconv -f ISO8859-1 -t UTF8 |\
-    sed "s/[\]'/'/g" |\
-    python -mjson.tool
+    sed "s/[\]'/'/g" 
+#    python -mjson.tool
 #    jsonlint -f
