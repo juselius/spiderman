c84fe164385e57c8a3076233c64c8fbafa185706
Jonas Juselius <jonas.juselius@uit.no>
2013-04-08 15:15:36 +0200

    Added sample css & stuff.

---
+++ data/js/preview.js
@@ -0,0 +1,29 @@
+function updatePreviewPane() {
+    $("#previewpane").hide();
+    var url = location.pathname.replace(/_edit\//,"_preview/");
+    $.post(
+      url,
+      {"raw" : $("#editedText").attr("value")},
+      function(data) {
+        $('#previewpane').html(data);
+        // Process any mathematics if we're using MathML
+        if (typeof(convert) == 'function') { convert(); }
+        // Process any mathematics if we're using jsMath
+        if (typeof(jsMath) == 'object')    { jsMath.ProcessBeforeShowing(); }
+        // Process any mathematics if we're using MathJax
+        if (typeof(window.MathJax) == 'object') {
+          // http://docs.mathjax.org/en/latest/typeset.html
+          var math = document.getElementById("MathExample");
+          MathJax.Hub.Queue(["Typeset",MathJax.Hub,math]);
+        }
+      },
+      "html");
+
+    $('#previewpane').fadeIn(1000);
+
+};
+$(document).ready(function(){
+    $("#previewButton").show();
+    $("#editedText").focus();
+});
+
---
+++ data/js/search.js
@@ -0,0 +1,28 @@
+jQuery.fn.highlightPattern = function (patt, className)
+{
+    // patt is a space separated list of strings - we want to highlight
+    // an occurrence of any of these strings as a separate word.
+    var regex = new RegExp('\\b(' + patt.replace(/ /, '|') + ')\\b', 'g');
+
+    return this.each(function ()
+    {
+        this.innerHTML = this.innerHTML.replace(regex,
+          '<span class=\'' + className + '\'>' + '$1' + '</span>');
+    });
+};
+function toggleMatches(obj) {
+  var pattern = $('#pattern').text();
+  var matches = obj.next('.matches')
+  matches.slideToggle(300);
+  matches.highlightPattern(pattern, 'highlighted');
+  if (obj.html() == '[show matches]') {
+      obj.html('[hide matches]');
+    } else {
+      obj.html('[show matches]');
+    };
+  }
+$(function() {
+  $('a.showmatch').attr('onClick', 'toggleMatches($(this));');
+  $('pre.matches').hide();
+  $('a.showmatch').show();
+  });
