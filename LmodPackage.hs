d235b42636b873273abeca472a9b81ee21d9c9b8
Jonas Juselius <jonas.juselius@uit.no>
2014-02-04 12:36:00 +0100

    Use applicatives instead of monads in LmodPackage.

--- LmodPackage.hs
+++ LmodPackage.hs
@@ -23,7 +23,7 @@ import qualified Data.Text as T
 import qualified Data.Vector as V
 
 -- | Lmod package representation
-data Package = Package 
+data Package = Package
     { packageName :: T.Text
     , displayName :: T.Text
     , defaultVersionName :: T.Text
@@ -37,12 +37,12 @@ data Package = Package
     , versions :: HM.HashMap T.Text Version
     , defaultVersion :: Version
     } deriving (Eq, Show, Data, Typeable)
- 
+
 -- | Package version representation
-data Version = Version 
+data Version = Version
     { versionSpec :: T.Text
     , versionName :: T.Text
-    , helpText :: T.Text 
+    , helpText :: T.Text
     , helpPageHref :: T.Text
     } deriving (Eq, Show, Data, Typeable)
 
@@ -57,7 +57,7 @@ data Packages = Packages
 instance FromJSON Packages where
   parseJSON (Array a) = do
     pack <- return $ V.toList (V.map fromJSON a) :: Parser [Result Package]
-    let good = HM.fromList $ map (\(Success x) -> 
+    let good = HM.fromList $ map (\(Success x) ->
             (packageName x, x)) $ filter isSuccess pack
         bad = map (\(n, Error x) -> (n, x)) $ filter isFailed (zip [1..] pack)
     return $ Packages good bad
@@ -70,42 +70,40 @@ isSuccess (Success _) = True
 isSuccess (Error _) = False
 
 instance FromJSON Package where
-    parseJSON (Object o) = 
-        Package <$> o .: "package"
+    parseJSON (Object o) = Package
+        <$> o .: "package"
         <*> o .: "displayName"
         <*> o .: "defaultVersionName"
-        <*> o .:? "description" .!= "No description" 
+        <*> o .:? "description" .!= "No description"
         <*> o .:? "url" .!= ""
         <*> o .:? "license" .!= ""
-        <*> liftM T.toLower (o .:? "categories" .!= "") 
-        <*> return ""
-        <*> return ""
-        <*> liftM (map T.strip . T.splitOn (T.pack ",") . T.toLower) 
-            (o .:? "keywords" .!= "")
-        <*> do  
-            v <- o .: "versions" 
-            vl <- liftM V.toList $ V.mapM parseJSON v :: Parser [Version]
-            return $ HM.fromList $ map (\x -> 
-                (versionSpec x, x)) vl :: Parser (HM.HashMap T.Text Version)
-        <*> return emptyVersion
-    parseJSON _ = mzero 
+        <*> liftA T.toLower (o .:? "categories" .!= "")
+        <*> pure ""
+        <*> pure ""
+        <*> liftA tok (o .:? "keywords" .!= "")
+        <*> liftA tov (o .: "versions")
+        <*> pure emptyVersion
+        where
+            tok = map T.strip . T.splitOn (T.pack ",") . T.toLower
+            tov = HM.fromList . V.toList . V.map (\x ->(versionSpec x, x))
+    parseJSON _ = mzero
 
 instance FromJSON Version where
-    parseJSON (Object o) = 
+    parseJSON (Object o) =
         Version <$> o .: "versionName"
-        <*> liftM T.toLower (o .: "full")
+        <*> liftA T.toLower (o .: "full")
         <*> o .:? "help" .!= ""
         <*> return ""
 
-emptyPackage = Package T.empty T.empty T.empty T.empty T.empty 
-    T.empty T.empty T.empty T.empty [T.empty] 
+emptyPackage = Package T.empty T.empty T.empty T.empty T.empty
+    T.empty T.empty T.empty T.empty [T.empty]
     (HM.fromList [(T.empty, emptyVersion)]) emptyVersion
 
-emptyVersion = Version T.empty T.empty T.empty T.empty 
+emptyVersion = Version T.empty T.empty T.empty T.empty
 
 -- | Fetch the default Version object from a package
 getDefaultVersion p =
     let x = HM.lookup (defaultVersionName p) (versions p) in
     fromMaybe emptyVersion x
 
-unspace = T.filter (/=' ') 
+unspace = T.filter (/=' ')
